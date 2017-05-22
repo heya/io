9be52ebf0257370ccc1942cf4830958962f33c8f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2017-05-22 00:08:22 -0500

    Added fetch as a possible default transport.

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,7 @@
 define(['heya-unit',
 	'./test-io', './test-promise',	'./test-jsonp', './test-load',
 	'./test-mock', './test-track', './test-cache', './test-bust',
-	'./test-io-as-bundle'],
+	'./test-io-as-bundle', './test-fetch'],
 function(unit){
 	'use strict';
 
