fb131bfdc0216a8f41dcc1a25518f7b79d890bee
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2017-06-16 20:27:15 -0500

    Removed unnecessary dependencies.

--- url.js
+++ url.js
@@ -1,5 +1,5 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(['./io', './scaffold'], function (io, scaffold) {
+([], function () {
 	'use strict';
 
 	return function url (parts) {
